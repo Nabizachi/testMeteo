470f441bf3a2abf7658913bcf16f69277e4fc785
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-20 17:37:26 +0700

    lib led_strip

---
+++ lib/led_strip/led_strip.h
@@ -0,0 +1,23 @@
+#ifndef __LED_STRIP_H__
+#define __LED_STRIP_H__
+
+#include "led_strip_encoder.h"
+#include "driver/rmt_tx.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define RMT_LED_STRIP_RESOLUTION_HZ 10000000 // 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+
+#define EXAMPLE_LED_NUMBERS 14
+#define EXAMPLE_CHASE_SPEED_MS 5000
+
+static uint8_t led_strip_pixels[EXAMPLE_LED_NUMBERS * 3];
+
+int gpioNum;
+
+void led_strip_hsv2rgb(uint32_t h, uint32_t s, uint32_t v, uint32_t *r, uint32_t *g, uint32_t *b);
+void stripLedBegin(int gpioNum);
+static uint8_t setPixelColorRGB(int number, uint8_t Red, uint8_t Blue, uint8_t Green, int brightness);
+static uint8_t setPixelColorHSV(int number, uint16_t color, uint32_t brightness);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include "led_strip.h"
+
+void startLed()
+{
+    stripLedBegin(26);
+}
+
+void app_main()
+{
+
+    setPixelColorHSV(0, 240, 1);
+    setPixelColorHSV(1, 60, 1);
+    setPixelColorHSV(2, 120, 1);
+    setPixelColorHSV(3, 200, 1);
+    setPixelColorHSV(4, 180, 1);
+    setPixelColorRGB(5, 255, 255, 255, 1);
+    setPixelColorRGB(6, 255, 100, 0, 1);
+    setPixelColorRGB(7, 255, 0, 0, 1);
+    setPixelColorRGB(8, 0, 255, 0, 1);
+    setPixelColorRGB(9, 0, 0, 255, 1);
+
+    vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
+    // memset(led_strip_pixels, 0, sizeof(led_strip_pixels));
+    // vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
+}
