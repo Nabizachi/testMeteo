61ba62b28976d9f6f96e5f92a6f182e79884149c
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-21 22:09:01 +0700

    working project

--- src/main.c
+++ src/main.c
@@ -1,25 +1,158 @@
 #include "stdio.h"
 #include "led_strip.h"
 
-void startLed()
-{
-    stripLedBegin();
-}
-
 void app_main()
 {
+    stripLedBegin();
+    for (int i = 1; i < 50; i++)
+    {
+        setPixelColorRGB(0, 255, 0, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(1, 255, 150, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(2, 255, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(3, 150, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(4, 0, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(5, 0, 255, 150, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(6, 0, 225, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(7, 0, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(8, 0, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(9, 150, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(10, 255, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(11, 255, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(12, 255, 255, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
 
-    setPixelColorHSV(0, 240, 1);
-    setPixelColorHSV(1, 60, 1);
-    setPixelColorHSV(2, 120, 1);
-    setPixelColorHSV(3, 200, 1);
-    setPixelColorHSV(4, 180, 1);
-    setPixelColorRGB(5, 255, 255, 255, 1);
-    setPixelColorRGB(6, 255, 100, 0, 1);
-    setPixelColorRGB(7, 255, 0, 0, 1);
-    setPixelColorRGB(8, 0, 255, 0, 1);
-    setPixelColorRGB(9, 0, 0, 255, 1);
-    vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
+        setPixelColorRGB(0, 255, 255, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(1, 255, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(2, 255, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(3, 150, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(4, 0, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(5, 0, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(6, 0, 225, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(7, 0, 255, 150, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(8, 0, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(9, 150, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(10, 255, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(11, 255, 150, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(12, 255, 0, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+    for (int i = 50; i > 1; i -= 1)
+    {
+        setPixelColorRGB(0, 255, 0, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(1, 255, 150, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(2, 255, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(3, 150, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(4, 0, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(5, 0, 255, 150, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(6, 0, 225, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(7, 0, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(8, 0, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(9, 150, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(10, 255, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(11, 255, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(12, 255, 255, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+        setPixelColorRGB(0, 255, 255, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(1, 255, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(2, 255, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(3, 150, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(4, 0, 0, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(5, 0, 150, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(6, 0, 225, 255, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(7, 0, 255, 150, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(8, 0, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(9, 150, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(10, 255, 255, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(11, 255, 150, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+        setPixelColorRGB(12, 255, 0, 0, i);
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+    /*for (;;)
+    {
+        for (int i = 1; i < 50; i++)
+        {
+            setPixelColorRGB(0, 255, 0, 0, i);
+            setPixelColorRGB(1, 255, 150, 0, i);
+            setPixelColorRGB(2, 255, 255, 0, i);
+            setPixelColorRGB(3, 150, 255, 0, i);
+            setPixelColorRGB(4, 0, 255, 0, i);
+            setPixelColorRGB(5, 0, 255, 150, i);
+            setPixelColorRGB(6, 0, 225, 255, i);
+            setPixelColorRGB(7, 0, 150, 255, i);
+            setPixelColorRGB(8, 0, 0, 255, i);
+            setPixelColorRGB(9, 150, 0, 255, i);
+            setPixelColorRGB(10, 255, 0, 255, i);
+            setPixelColorRGB(11, 255, 150, 255, i);
+            setPixelColorRGB(12, 255, 255, 255, i);
+            vTaskDelay(pdMS_TO_TICKS(50));
+        }
+        for (int i = 50; i > 1; i -= 1)
+        {
+            setPixelColorRGB(0, 255, 0, 0, i);
+            setPixelColorRGB(1, 255, 150, 0, i);
+            setPixelColorRGB(2, 255, 255, 0, i);
+            setPixelColorRGB(3, 150, 255, 0, i);
+            setPixelColorRGB(4, 0, 255, 0, i);
+            setPixelColorRGB(5, 0, 255, 150, i);
+            setPixelColorRGB(6, 0, 225, 255, i);
+            setPixelColorRGB(7, 0, 150, 255, i);
+            setPixelColorRGB(8, 0, 0, 255, i);
+            setPixelColorRGB(9, 150, 0, 255, i);
+            setPixelColorRGB(10, 255, 0, 255, i);
+            setPixelColorRGB(11, 255, 150, 255, i);
+            setPixelColorRGB(12, 255, 255, 255, i);
+            vTaskDelay(pdMS_TO_TICKS(50));
+        }
+    }*/
     // memset(led_strip_pixels, 0, sizeof(led_strip_pixels));
     // vTaskDelay(pdMS_TO_TICKS(EXAMPLE_CHASE_SPEED_MS));
 }
